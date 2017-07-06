28a7061470ebf35d3078316746d1ca29a5f48558
glwagner <wagner.greg@gmail.com>
2017-07-06 18:53:58 -0400

    Removes dependency on run.sh and ensures creation of build and run dirs

--- scripts/cleancompilerun.sh
+++ scripts/cleancompilerun.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
+# Number of processors for making and running
+npmake=2
+nprun=1
+
+gcmdir="/Users/glwagner/Software/MITgcm"
+optfile="$gcmdir/tools/build_options/neve"
+
 # Clean up build and run directory
-rm -r ./build/*
-rm -r ./run/*
+rm -r ./build
+rm -r ./run
+mkdir build
+mkdir run
 
 # Compile
-gcmdir='/data5/glwagner/gcm/MITgcm'
-optfile="$gcmdir/tools/build_options/sverdrup_glwagner"
-
 cd ./build
 
-$gcmdir/tools/genmake2 \
-    -optfile=$optfile \
-    -mods=../code/ \
-    -enable=mnc \
-    -rootdir=$gcmdir/
+if [ $nprun -eq 1 ]
+then # do not compile with mpi:
+    $gcmdir/tools/genmake2 \
+        -optfile=$optfile \
+        -mods=../code/ \
+        -enable=mnc \
+        -rootdir=$gcmdir/
+else # compile with mpi:
+    $gcmdir/tools/genmake2 \
+        -optfile=$optfile \
+        -enable=mnc \
+        -mpi \
+        -mods=../code/ \
+        -rootdir=$gcmdir/
+fi
 
 make depend
-make -j8
+make -j$npmake
 
 # Run
-cd ../
-./run.sh
+cd ../run
+ln -s ../input/* .
+
+if [ $nprun -eq 1 ]
+then
+    ../build/mitgcmuv > output.txt
+else
+    mpiexec -n $nprun ../build/mitgcmuv > output.txt
+fi
+
